bc8931a7150a9b4a2db119784a3e154be0ed6af6
yt-xmati <93664892+yt-xmati@users.noreply.github.com>
2022-01-27 13:04:48 +0100

    accuracy text change

---
+++ accuracy text.lua
@@ -0,0 +1,50 @@
+function onUpdate()
+    if ratingName == 'Perfect!!' then
+        setRatingName('I BROKE MY PHONE!!')
+    end
+
+
+    if ratingName == 'Sick!' then
+        setRatingName('bambi says 79.111.246.10')
+    end
+
+
+    if ratingName == 'Good' then
+        setRatingName('even cool')
+    end
+
+
+    if ratingName == 'Nice' then
+        setRatingName('ratio')
+    end
+
+
+    if ratingName == 'Meh' then
+        setRatingName('balls')
+    end
+
+
+    if ratingName == 'Bruh' then
+        setRatingName('me when bad:')
+    end
+
+
+    if ratingName == 'Bad' then
+        setRatingName('no')
+    end
+
+
+    if ratingName == 'Shit' then
+        setRatingName('what the fuck')
+    end
+
+
+    if ratingName == 'You Suck!' then
+        setRatingName('HOLY FUCKING SHIT')
+    end
+
+
+    if ratingName == 'Great' then
+        setRatingName('ok')
+    end
+end
